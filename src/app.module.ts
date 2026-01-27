2d8fbdcc9ab3c99b0e56ba5af0c17a49a3de4a8a
gopall-93 <gopallsharma93@gmail.com>
2026-01-28 04:32:14 +0530

    Configure global guards, Swagger, and Prisma
    Integrates Prisma into the core module and updates the main entry point to include Swagger documentation for better API visibility.
    
    Implements global validation pipes and security guards for JWT authentication and role-based access control to ensure consistent protection across all endpoints.
    
    Refactors the root controller to provide a database connection health check.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,26 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { APP_GUARD } from '@nestjs/core';
+
+import { PrismaModule } from './prisma/prisma.module';   // ⭐ ADD THIS
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { CustomersModule } from './customers/customers.module';
 import { TasksModule } from './tasks/tasks.module';
 
+import { JwtAuthGuard } from './common/guards/jwt-auth.guard';
+import { RolesGuard } from './common/guards/roles.guard';
+
 @Module({
-  imports: [AuthModule, UsersModule, CustomersModule, TasksModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    PrismaModule,
+    AuthModule,
+    UsersModule,
+    CustomersModule,
+    TasksModule,
+  ],
+  providers: [
+    { provide: APP_GUARD, useClass: JwtAuthGuard },
+    { provide: APP_GUARD, useClass: RolesGuard },
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { JwtAuthGuard } from './common/guards/jwt-auth.guard';
+import { RolesGuard } from './common/guards/roles.guard';
+import { Reflector } from '@nestjs/core';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Mini CRM API')
+    .setDescription('Backend assignment API documentation')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  const reflector = app.get(Reflector);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
